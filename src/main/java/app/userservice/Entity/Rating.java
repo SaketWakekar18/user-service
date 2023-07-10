b0ba47479308e6d756722a9d0d40598f88a50311
SaketWakekar18 <saketwakekar@gmail.com>
2023-07-10 13:05:37 +0530

    Okta implemented

--- src/main/java/app/userservice/Entity/Rating.java
+++ src/main/java/app/userservice/Entity/Rating.java
@@ -1,19 +1,17 @@
 package app.userservice.Entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Rating {
-
-    private String ratingId;
-    private String userId;
-    private String hotelId;
+    private int ratingId;
+    private int userId;
+    private int hotelId;
     private int rating;
     private String feedback;
+    private Hotels hotels;
 }
--- src/test/java/app/userservice/UserServiceApplicationTests.java
+++ src/test/java/app/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package app.userservice;
 
+import app.userservice.Entity.Rating;
+import app.userservice.External.Services.RatingService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
+	@Autowired
+    private RatingService ratingService;
+
+    @Test
+    void contextLoads() {
+    }
 
 	@Test
-	void contextLoads() {
+    void createRating() {
+		Rating rating = Rating.builder()
+				.rating(10)
+				.userId(6)
+				.hotelId(1)
+				.feedback("This is created using feign client")
+				.build();
+		Rating serviceRating = ratingService.createRating(rating);
+		System.out.println("Service Rating: "+ serviceRating);
 	}
 
 }
